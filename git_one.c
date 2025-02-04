8b611b622e9e5904aaa97a53b812487e4a83f04c
Amina3524 <na_meliani@esi.dz>
2025-02-04 12:45:15 +0100

    local update

--- git_one.c
+++ git_one.c
@@ -8,7 +8,7 @@ int main() {
      scanf("%s", &name);
      printf("hello, %s.I hope you are good!", name) ;
      printf("we fixed an error here for example");
-     printf("this change is made in GitHub!!");
+     printf("this change is made in the local repo!!");
      printf("fix");
 
 }
